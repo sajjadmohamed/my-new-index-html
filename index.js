b4f3f6df6d9bba5f1aabd4684ac1260d31572972
Sajjad Mohamed <sajjadmohamed2002@gmail.com>
2022-01-19 22:57:41 +0300

    sajjad

---
+++ index.js
@@ -0,0 +1,15 @@
+function rasuol() {
+	let sajjad1 = "Hi this is Const, 10 + 10 =?";
+	const x = 10;
+	const z = 10;
+	let y = x + z;
+
+	document.getElementById("demo").innerHTML = sajjad1 + "<h2>" + y;
+}
+
+function sajjad() {
+	let sajjad2 = "this is Date";
+	document.getElementById("demo1").innerHTML = sajjad2 + "<h2>" + Date();
+}
+//mix = confirm("hhuhih");
+//alert(mix)
